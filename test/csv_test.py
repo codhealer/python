a072c940f557678aaca4bbc0f47eda8969ffd989
chenbin <chenbin@chenbindeMac-mini.local>
2018-07-02 23:16:20 +0800

    修改python环境变量

--- test/csv_test.py
+++ test/csv_test.py
@@ -1,5 +1,7 @@
-import csv
-fp = open('../files/test.csv','w+') 
+import csv,os
+path = os.getcwd()
+print(path)
+fp = open(path+'/files/test.csv','w+') 
 writer = csv.writer(fp) 
 writer.writerow(('id','name'))
 writer.writerow(('id','name'))
