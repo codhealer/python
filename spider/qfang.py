b9bfd3e2af9216505af41b8c787b36fbd3c23b75
chenbin <chenbin@chenbindeMac-mini.local>
2018-07-03 22:51:09 +0800

    修改文件

--- spider/qfang.py
+++ spider/qfang.py
@@ -5,12 +5,9 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 #from spider.httpUtil import getHeader
-from spider import httpUtil
+from utils import httpUtil
 #也可以写程from spider.httpUtil import getHeader
-def init():
-    pypath = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../utils')
-    sys.path.append(pypath)
-init()
+
 
 def get_links(url,pattern):
     wb_data = requests.get(url,headers=httpUtil.getHeader())
---
+++ spider/zhipin.py
@@ -0,0 +1,6 @@
+import requests
+import re
+import time
+import os
+from ..utils import httpUtils
+print(os.getcwd())
