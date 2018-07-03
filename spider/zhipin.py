75aab8f62796caade84ad08dd5e262dba85e9e92
chenbin <chenbin@chenbindeMac-mini.local>
2018-07-03 23:09:49 +0800

    获取直聘网内容

--- spider/zhipin.py
+++ spider/zhipin.py
@@ -2,5 +2,8 @@ import requests
 import re
 import time
 import os
-from ..utils import httpUtils
-print(os.getcwd())
+from utils import httpUtils
+def get_links(url,pattern):
+    wb_data = requests.get(url,headers=httpUtils.getHeader())
+    print(wb_data)
+get_links('https://www.zhipin.com/c101280600/?page=1&ka=page-2','')
